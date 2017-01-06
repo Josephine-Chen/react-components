bfee30f7b5783c928c585256782710f132e1980e
Josephine Chen <jychen@g.hmc.edu>
2017-01-06 12:20:34 -0800

    Created grocery list with hover functionality

--- app.js
+++ app.js
@@ -1,1 +1,60 @@
-// TODO
+class GroceryListItem extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false,
+      hover: false
+    };
+  }
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+  onMouseEnterHandler() {
+    this.setState({
+      hover: true
+    });
+  }
+  onMouseLeaveHandler() {
+    this.setState({
+      hover: false
+    });
+  }
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none',
+      fontWeight: this.state.hover ? 'bold' : 'normal',
+    };
+
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}
+      onMouseEnter={this.onMouseEnterHandler.bind(this)}
+      onMouseLeave={this.onMouseLeaveHandler.bind(this)}>{
+        this.props.groceryItem}
+        </li>
+      );
+  }
+}
+
+var GroceryList = (props) => (
+  <ul>
+      {props.groceryItems.map(groceryItem =>
+        <GroceryListItem groceryItem={groceryItem} />
+        )}
+  </ul>
+);
+
+var App = () => (
+  <div>
+  <h2>Grocery List</h2>
+  <GroceryList groceryItems={['Bread', 'Eggs', 'Chicken']}/>
+  </div>
+);
+
+var Bread = () => (<div>Bread</div>);
+
+var Eggs = () => (<div>Eggs</div>);
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
